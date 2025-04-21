3eb4ea7f05f532925be2cc061567483f4daf6620
tarang kalariya <tarangkalaria55@gmail.com>
2025-04-21 17:05:05 +0530

    apex chart implemented

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,25 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  // importProvidersFrom,
+} from '@angular/core';
 import {
   provideRouter,
   withComponentInputBinding,
   withRouterConfig,
 } from '@angular/router';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideRdxDialogConfig } from '@radix-ng/primitives/dialog';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding(), withRouterConfig({})),
+    provideHttpClient(),
     provideRdxDialogConfig(),
+    // importProvidersFrom(),
   ],
 };
--- src/app/pages/non-admin/dashboard/dashboard.component.ts
+++ src/app/pages/non-admin/dashboard/dashboard.component.ts
@@ -6,19 +6,156 @@ import {
   type OnInit,
 } from '@angular/core';
 
+import { OwlDateTimeModule } from '@danielmoncada/angular-datetime-picker';
+import { OwlMomentDateTimeModule } from '@danielmoncada/angular-datetime-picker-moment-adapter';
+
+import {
+  NgApexchartsModule,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexDataLabels,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexXAxis,
+  ApexFill,
+} from 'ng-apexcharts';
+
+export interface ChartOptions {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  yaxis: ApexYAxis;
+  xaxis: ApexXAxis;
+  fill: ApexFill;
+  title: ApexTitleSubtitle;
+}
+
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'main[app-dashboard]',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [OwlDateTimeModule, OwlMomentDateTimeModule, NgApexchartsModule],
   host: {
     class: 'flex-grow pt-24 pb-20',
   },
 })
 export class DashboardComponent implements OnInit {
   readonly #destroyRef = inject(DestroyRef);
+
+  public chartOptions: ChartOptions;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: 'Inflation',
+          data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2],
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'bar',
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            position: 'top', // top, center, bottom
+          },
+        },
+      },
+      dataLabels: {
+        enabled: true,
+        formatter: function (val) {
+          return val + '%';
+        },
+        offsetY: -20,
+        style: {
+          fontSize: '12px',
+          colors: ['#304758'],
+        },
+      },
+      xaxis: {
+        categories: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec',
+        ],
+        position: 'bottom',
+        // labels: {
+        //   offsetY: 5, //-18,
+        // },
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+        crosshairs: {
+          fill: {
+            type: 'gradient',
+            gradient: {
+              colorFrom: '#D8E3F0',
+              colorTo: '#BED1E6',
+              stops: [0, 100],
+              opacityFrom: 0.4,
+              opacityTo: 0.5,
+            },
+          },
+        },
+        tooltip: {
+          enabled: true,
+          offsetY: -35,
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'light',
+          type: 'horizontal',
+          shadeIntensity: 0.25,
+          gradientToColors: undefined,
+          inverseColors: true,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [50, 0, 100, 100],
+        },
+      },
+      yaxis: {
+        axisBorder: {
+          show: false,
+        },
+        axisTicks: {
+          show: false,
+        },
+        labels: {
+          show: false,
+          formatter: function (val) {
+            return val + '%';
+          },
+        },
+      },
+      title: {
+        text: 'Monthly Inflation in Argentina, 2002',
+        // offsetY: 320,
+        align: 'center',
+        style: {
+          color: '#444',
+        },
+      },
+    };
+  }
 
   ngOnInit(): void {}
 }
