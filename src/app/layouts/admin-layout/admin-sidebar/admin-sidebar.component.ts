27178ccdc387d6c89c1394451be89e19d2c0aacd
tarang kalariya <tarangkalaria55@gmail.com>
2025-04-21 12:02:29 +0530

    -

--- src/app/layouts/admin-layout/admin-sidebar/admin-sidebar.component.ts
+++ src/app/layouts/admin-layout/admin-sidebar/admin-sidebar.component.ts
@@ -1,8 +1,13 @@
+// import { NgClass } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
   DestroyRef,
+  effect,
+  ElementRef,
   inject,
+  Renderer2,
+  signal,
   type OnInit,
 } from '@angular/core';
 
@@ -11,14 +16,50 @@ import {
   templateUrl: './admin-sidebar.component.html',
   styleUrl: './admin-sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  // imports: [NgClass],
   host: {
     class:
-      'relative flex flex-col h-screen bg-[#111111] border-r border-gray-800 text-white transition-all duration-300 w-[240px]',
+      'sidebar relative flex flex-col h-screen bg-[#111111] border-r border-gray-800 text-white transition-all duration-300',
   },
 })
 export class AdminSidebarComponent implements OnInit {
   readonly #destroyRef = inject(DestroyRef);
 
-  ngOnInit(): void {}
+  #elRef = inject(ElementRef);
+  #renderer = inject(Renderer2);
+
+  isOpen = signal(false);
+
+  constructor() {
+    effect(() => {
+      if (this.#elRef.nativeElement) {
+        if (this.isOpen()) {
+          // eslint-disable-next-line no-debugger
+          debugger;
+          this.#renderer.setAttribute(
+            this.#elRef.nativeElement,
+            'data-sidebar-open',
+            'true',
+          );
+          this.#renderer.removeClass(this.#elRef.nativeElement, 'w-[60px]');
+          this.#renderer.addClass(this.#elRef.nativeElement, 'w-[240px]');
+        } else {
+          this.#renderer.removeAttribute(
+            this.#elRef.nativeElement,
+            'data-sidebar-open',
+          );
+          this.#renderer.removeClass(this.#elRef.nativeElement, 'w-[240px]');
+          this.#renderer.addClass(this.#elRef.nativeElement, 'w-[60px]');
+        }
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.toggleMenu();
+  }
+
+  toggleMenu() {
+    this.isOpen.update(x => !x);
+  }
 }
