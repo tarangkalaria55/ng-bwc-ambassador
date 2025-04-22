f8c982a5db1ce8dbf1b7d0e57f1ae97f5bc8112f
Tarang Kalariya <tarangappliquer@gmail.com>
2025-04-22 16:27:49 +0530

    titles added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,6 +18,7 @@ export const routes: Routes = [
       },
       {
         path: 'dashboard',
+        title: 'Dashboard',
         loadComponent: () =>
           import('./pages/non-admin/dashboard/dashboard.component').then(
             m => m.DashboardComponent,
@@ -25,6 +26,7 @@ export const routes: Routes = [
       },
       {
         path: 'influencer-performance',
+        title: 'Influencer performance',
         loadComponent: () =>
           import(
             './pages/non-admin/influencer-performance/influencer-performance.component'
@@ -32,6 +34,7 @@ export const routes: Routes = [
       },
       {
         path: 'profile',
+        title: 'Profile',
         loadComponent: () =>
           import('./pages/non-admin/profile/profile.component').then(
             m => m.ProfileComponent,
@@ -39,6 +42,7 @@ export const routes: Routes = [
       },
       {
         path: 'ambassador',
+        title: 'Ambassador',
         loadComponent: () =>
           import('./pages/non-admin/ambassador/ambassador.component').then(
             m => m.AmbassadorComponent,
@@ -46,6 +50,7 @@ export const routes: Routes = [
       },
       {
         path: 'earnings-history',
+        title: 'Earning history',
         loadComponent: () =>
           import(
             './pages/non-admin/earnings-history/earnings-history.component'
@@ -61,6 +66,7 @@ export const routes: Routes = [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       {
         path: 'dashboard',
+        title: 'Dashboard',
         loadComponent: () =>
           import('./pages/admin/dashboard/dashboard.component').then(
             m => m.DashboardComponent,
@@ -69,6 +75,7 @@ export const routes: Routes = [
 
       {
         path: 'applications',
+        title: 'Applications',
         loadComponent: () =>
           import('./pages/admin/applications/applications.component').then(
             m => m.ApplicationsComponent,
@@ -77,6 +84,7 @@ export const routes: Routes = [
 
       {
         path: 'influencers',
+        title: 'Influencers',
         children: [
           {
             path: '',
@@ -87,6 +95,7 @@ export const routes: Routes = [
           },
           {
             path: 'add',
+            title: 'Add influencer',
             loadComponent: () =>
               import(
                 './pages/admin/influencers/influencer-add/influencer-add.component'
@@ -94,6 +103,7 @@ export const routes: Routes = [
           },
           {
             path: ':id',
+            title: 'Influencer detail',
             children: [
               {
                 path: '',
@@ -104,6 +114,7 @@ export const routes: Routes = [
               },
               {
                 path: 'edit',
+                title: 'Edit influencer',
                 loadComponent: () =>
                   import(
                     './pages/admin/influencers/influencer-edit/influencer-edit.component'
@@ -116,6 +127,7 @@ export const routes: Routes = [
 
       {
         path: 'influencer-code-details',
+        title: 'Influencer code details',
         loadComponent: () =>
           import(
             './pages/admin/influencer-code-details/influencer-code-details.component'
@@ -124,6 +136,7 @@ export const routes: Routes = [
 
       {
         path: 'admin-invoices',
+        title: 'Admin invoices',
         children: [
           {
             path: '',
@@ -134,6 +147,7 @@ export const routes: Routes = [
           },
           {
             path: ':invoiceId',
+            title: 'Admin invoice detail',
             loadComponent: () =>
               import(
                 './pages/admin/admin-invoices/admin-invoice-detail/admin-invoice-detail.component'
@@ -144,6 +158,7 @@ export const routes: Routes = [
 
       {
         path: 'active-codes',
+        title: 'Active codes',
         loadComponent: () =>
           import('./pages/admin/active-codes/active-codes.component').then(
             m => m.ActiveCodesComponent,
@@ -152,6 +167,7 @@ export const routes: Routes = [
 
       {
         path: 'code-details/:code',
+        title: 'Code details',
         loadComponent: () =>
           import('./pages/admin/code-details/code-details.component').then(
             m => m.CodeDetailsComponent,
@@ -160,6 +176,7 @@ export const routes: Routes = [
 
       {
         path: 'welcome-email',
+        title: 'Welcome email',
         loadComponent: () =>
           import('./pages/admin/welcome-email/welcome-email.component').then(
             m => m.WelcomeEmailComponent,
@@ -174,6 +191,7 @@ export const routes: Routes = [
     children: [
       {
         path: '',
+        title: 'Page not found',
         loadComponent: () =>
           import('./pages/not-found/not-found.component').then(
             m => m.NotFoundComponent,
